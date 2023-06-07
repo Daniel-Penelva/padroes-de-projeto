ed0fb9167fc39b15fc111b94f66aa4d26c900d35
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-07 02:39:01 -0300

    Implementação da fábrica de personagens da facção Heróis

---
+++ padroesDeProjetos/src/padroesDeProjetos/abstractFactory/FabricaHeroisImpl.java
@@ -0,0 +1,25 @@
+package padroesDeProjetos.abstractFactory;
+
+// Implementação da fábrica de personagens da facção Heróis
+public class FabricaHeroisImpl implements FabricaHerois {
+
+	@Override
+	public Personagem criarPersonagem() {
+		
+		// Implementação padrão para criar personagens na facção heróis
+		return criarGuerreiro();
+	}
+
+	@Override
+	public Personagem criarGuerreiro() {
+		
+		return new GuerreiroHeroi();
+	}
+
+	@Override
+	public Personagem criarMago() {
+		
+		return new MagoHeroi();
+	}
+
+}
