5268c03ce578e9b53a77f56e8aad49edc5985df2
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-11 01:42:28 -0300

    Factory Method -criando a classe abstrata "FabricaVeiculo" q define o método factoryMethod()

---
+++ padroesDeProjetos/src/padroesDeProjetos/factoryMethod/FabricaVeiculos.java
@@ -0,0 +1,10 @@
+package padroesDeProjetos.factoryMethod;
+
+public abstract class FabricaVeiculos {
+	
+	// Método factoryMethod para criar o veículo
+	public abstract Veiculo factoryMethod();
+	
+	// Outros métodos da fábrica ...
+
+}
