def7d96171d293013b7e9857b08c3c456d656f66
Daniel Penelva <d4n.andrade@gmail.com>
2023-07-02 02:28:14 -0300

    Composite - Criar classe Departamento

---
+++ padroesDeProjetos/src/padroesDeProjetos/composite/Departamento.java
@@ -0,0 +1,36 @@
+package padroesDeProjetos.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Departamento implements Component{
+	
+	private String nome;
+	private List<Component> componentes;
+	
+	
+
+	public Departamento(String nome) {
+		this.nome = nome;
+		this.componentes = new ArrayList<Component>();
+	}
+	
+	public void adicionarComponente(Component componente) {
+		componentes.add(componente);
+	}
+	
+	public void removerComponente(Component componente) {
+		componentes.remove(componente);
+	}
+
+	@Override
+	public void showDetails() {
+		System.out.println("Departamento: " + nome);
+		System.out.println("Componentes:");
+		
+		for (Component component : componentes) {
+			component.showDetails();
+		}
+		
+	}
+}
