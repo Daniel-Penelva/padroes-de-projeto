0fbce90dce6742bfe320f0612c819ed0d6343585
Daniel Penelva <d4n.andrade@gmail.com>
2023-07-09 01:11:48 -0300

    Criando as classes ReprodutorDeAudio e ReprodutorDeVideo

---
+++ padroesDeProjetos/src/padroesDeProjetos/facade/ReprodutorDeAudio.java
@@ -0,0 +1,8 @@
+package padroesDeProjetos.facade;
+
+public class ReprodutorDeAudio {
+
+	public void reproduzir(String arquivo) {
+		System.out.println("Reproduzindo vídeo: " + arquivo);
+	}
+}
---
+++ padroesDeProjetos/src/padroesDeProjetos/facade/ReprodutorDeVideo.java
@@ -0,0 +1,8 @@
+package padroesDeProjetos.facade;
+
+public class ReprodutorDeVideo {
+
+	public void reproduzir(String arquivo) {
+		System.out.println("Reproduzindo áudio: " + arquivo);
+	}
+}
