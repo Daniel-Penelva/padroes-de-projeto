b631ba4e3a35598c610054f8f412112ba25fe80e
Daniel Penelva <d4n.andrade@gmail.com>
2023-07-09 01:16:01 -0300

    criando as classes CodificadorDeAudio e CodificadorDeVideo

---
+++ padroesDeProjetos/src/padroesDeProjetos/facade/CodificadorDeAudio.java
@@ -0,0 +1,8 @@
+package padroesDeProjetos.facade;
+
+public class CodificadorDeAudio {
+	
+	public void codificar(String arquivo) {
+		System.out.println("Codificando áudio: " + arquivo);
+	}
+}
---
+++ padroesDeProjetos/src/padroesDeProjetos/facade/CodificadorDeVideo.java
@@ -0,0 +1,8 @@
+package padroesDeProjetos.facade;
+
+public class CodificadorDeVideo {
+	
+	public void codificar(String arquivo) {
+		System.out.println("Codificando vídeo: " + arquivo);
+	}
+}
